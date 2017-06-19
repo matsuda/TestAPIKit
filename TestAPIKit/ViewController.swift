d923e26d1e84047a78a02017fdd265b0adc14c67
matsuda <kosukematsuda@gmail.com>
2017-06-19 16:59:34 +0900

    create project

---
+++ TestAPIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAPIKit
+//
+//  Created by kosuke.matsuda on 2017/06/19.
+//  Copyright © 2017年 Galapagos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
